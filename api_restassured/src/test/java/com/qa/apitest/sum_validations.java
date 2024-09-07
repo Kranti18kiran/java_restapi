0c37b7991b1b639bd8bbbc67559d59cfd0bc7562
Kranti18kiran <nanikiri@gmail.com>
2024-09-07 22:02:16 +1000

    adding assert

--- api_restassured/src/test/java/com/qa/apitest/sum_validations.java
+++ api_restassured/src/test/java/com/qa/apitest/sum_validations.java
@@ -1,5 +1,8 @@
 package com.qa.apitest;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import files.payload;
@@ -11,13 +14,19 @@ public class sum_validations {
 
         JsonPath jsTest = new JsonPath(payload.Courseprice());
         int countcourse = jsTest.getInt("courses.size()");
+        int sum = 0;
         for (int i = 0; i < countcourse; i++) {
             int courseprice = jsTest.get("courses[" + i + "].price");
             int coursecopies = jsTest.get("courses[" + i + "].copies");
             int amount = courseprice * coursecopies;
             System.out.println(amount);
+            sum = sum + amount;
 
         }
+        System.out.println(sum);
+        int amountpurchase = jsTest.getInt("dashboard.purchaseAmount");
+        System.out.println(amountpurchase);
+        Assert.assertEquals(sum, amountpurchase);
 
     }
 }

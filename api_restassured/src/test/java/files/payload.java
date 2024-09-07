9c41ebf9a1d248d97adbe5f9bb4c98f819695d9d
Kranti18kiran <nanikiri@gmail.com>
2024-09-07 22:10:43 +1000

    adding value to schema

--- api_restassured/src/test/java/files/payload.java
+++ api_restassured/src/test/java/files/payload.java
@@ -8,7 +8,7 @@ public class payload {
     }
 
     public static String Courseprice() {
-        return "{\n\n\"dashboard\": {\n\n\"purchaseAmount\": 910,\n\n\"website\": \"rahulshettyacademy.com\"\n\n},\n\n\"courses\": [\n\n{\n\n\"title\": \"Selenium Python\",\n\n\"price\": 50,\n\n\"copies\": 6\n\n},\n\n{\n\n\"title\": \"Cypress\",\n\n\"price\": 40,\n\n\"copies\": 4\n\n},\n\n{\n\n\"title\": \"RPA\",\n\n\"price\": 45,\n\n\"copies\": 10\n\n}\n\n]\n\n}";
+        return "{\n\n\"dashboard\": {\n\n\"purchaseAmount\": 3370,\n\n\"website\": \"rahulshettyacademy.com\"\n\n},\n\n\"courses\": [\n\n{\n\n\"title\": \"Selenium Python\",\n\n\"price\": 50,\n\n\"copies\": 6\n\n},\n\n{\n\n\"title\": \"Cypress\",\n\n\"price\": 40,\n\n\"copies\": 4\n\n},\n\n{\n\n\"title\": \"RPA\",\n\n\"price\": 45,\n\n\"copies\": 10\n\n},\n  {\n\n\"title\": \"playWright\",\n\n\"price\": 123,\n\n\"copies\": 20\n\n}\n\n]\n\n}";
     }
 
 }
